fd41f07cb6064846b44893f627a15fb4f46fe103
Rom0105 <savosko.photo@gmail.com>
2021-08-22 17:57:50 +0300

    правки

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import './sass/main.scss';
 import usersList from './templates/search-users.hbs';
 import { error } from '../node_modules/@pnotify/core/dist/PNotify.js';
 import '@pnotify/core/dist/BrightTheme.css';
+import axios from 'axios';
+import { result } from 'lodash';
 
 const refs = {
   form: document.querySelector('#form'),
@@ -10,19 +12,21 @@ const refs = {
   container: document.querySelector('.js-container'),
 };
 
-refs.form.addEventListener('submit', handlerSubmit);
+refs.form.addEventListener('submit', gitHandlerSubmit);
 refs.btn.addEventListener('click', clearBtn);
 refs.input.addEventListener('input', clearBtn);
 
-function handlerSubmit(e) {
+function gitHandlerSubmit(e) {
   e.preventDefault();
   const value = refs.input.value;
 
   clearMarkup();
   clear();
 
-  fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${value}`)
-    .then(response => response.json())
+  axios
+    .get(
+      `https://api.github.com/search/users?q=${value}&client_id=6a927e9749baf5170662&client_secret=5181dc33b7d4e79a04eb08a72ef4afca511c808a`,
+    )
     .then(users => errorCocktail(users))
     .catch(err => console.log(err));
 }
@@ -50,9 +54,10 @@ function clearBtn() {
 }
 
 function errorCocktail(users) {
-  if (users.drinks) {
-    markup(users.drinks);
-  } else {
+  if (users.data.items) {
+    markup(users.data.items);
+  }
+  if (users.data.total_count === 0) {
     error({
       text: 'Users not found!',
       delay: 2000,
